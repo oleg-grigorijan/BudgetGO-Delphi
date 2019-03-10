5affe3587e6cf5728a4547d6c6b57fcf2efa18a4
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-03-10 16:37:22 +0300

    TOperationList is created

--- budgetGO.dpr
+++ budgetGO.dpr
@@ -7,11 +7,18 @@ program budgetGO;
 uses
   System.SysUtils,
   UnitTOperation in 'models\UnitTOperation.pas',
-  UnitTOperationListNode in 'models\UnitTOperationListNode.pas';
+  UnitTOperationListNode in 'models\UnitTOperationListNode.pas',
+  UnitTOperationList in 'models\UnitTOperationList.pas';
+
+var
+  operList: TOperationList;
+  operTmp: POperation;
 
 begin
   try
-
+    operList := TOperationList.create;
+    readln;
+    operList.destroy()
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
---
+++ models/UnitTOperationList.pas
@@ -0,0 +1,159 @@
+unit UnitTOperationList;
+
+interface
+
+uses
+  System.SysUtils,
+  UnitTOperationListNode,
+  UnitTOperation;
+
+type
+  TOperationList = class
+    protected
+      head: TOperationListNode;
+      last: TOperationListNode;
+    public
+      count: Longword;
+      //constructor create(const filename: string); overload;
+      //destructor destroy();
+      function getItemsMaxId(): Longword;
+      procedure addNode(const item: POperation);
+      function getItem(const id: Longword): POperation;
+      function removeNode(const id: Longword;
+        const destroyItem: Boolean = true): Boolean;
+      procedure consoleOutput();
+  end;
+
+implementation
+
+function TOperationList.getItemsMaxId(): Longword;
+var
+  nodeCurr: TOperationListnode;
+begin
+  result := 0;
+  nodeCurr := self.head;
+  while nodeCurr <> nil do
+  begin
+    if nodeCurr.item^.id > result then
+      result := nodeCurr.item^.id;
+    nodeCurr := nodeCurr.next;
+  end;
+end;
+
+procedure TOperationList.addNode(const item: POperation);
+var
+  node, nodeCurr: TOperationListNode;
+begin
+  node := TOperationListNode.create(item);
+  nodeCurr := self.last;
+  while (nodeCurr <> nil) and
+    (nodeCurr.item^.date > item^.date) do
+  begin
+    nodeCurr := nodeCurr.prev;
+  end;
+  if nodeCurr <> nil then
+  begin
+    if nodeCurr.next = nil then
+      self.last := node
+    else
+    begin
+      node.next := nodeCurr.next;
+      (nodeCurr.next).prev := node;
+    end;
+    node.prev := nodeCurr;
+    nodeCurr.next := node;
+  end
+  else
+  begin
+    if self.head = nil then
+      self.last := node
+    else
+    begin
+      node.next := self.head;
+      (self.head).prev := node;
+    end;
+    self.head := node;
+  end;
+  inc(self.count);
+end;
+
+function TOperationList.getItem(const id: Longword): POperation;
+var
+  nodeCurr: TOperationListNode;
+begin
+  result := nil;
+  nodeCurr := self.head;
+  while nodeCurr <> nil do
+  begin
+    if nodeCurr.item^.id = id then
+    begin
+      result := nodeCurr.item;
+      break;
+    end;
+    nodeCurr := nodeCurr.next;
+  end;
+end;
+
+function TOperationList.removeNode(const id: Longword;
+  const destroyItem: Boolean = true): Boolean;
+var
+  nodeCurr: TOperationListNode;
+begin
+  result := false;
+  nodeCurr := self.head;
+  while nodeCurr <> nil do
+  begin
+    if nodeCurr.item^.id = id then
+    begin
+      if nodeCurr.prev <> nil then
+      begin
+        (nodeCurr.prev).next := nodeCurr.next;
+        if nodeCurr.next = nil then
+          self.last := nodeCurr.prev
+        else
+          (nodeCurr.next).prev := nodeCurr.prev;
+      end
+      else
+      begin
+        self.head := nodeCurr.next;
+        if self.head = nil then
+          self.last := nil
+        else
+          (self.head).prev := nil
+      end;
+      if destroyItem then
+        dispose(nodeCurr.item);
+      nodeCurr.destroy();
+      dec(self.count);
+      result := true;
+      break;
+    end;
+    nodeCurr := nodeCurr.next;
+  end;
+end;
+
+procedure TOperationList.consoleOutput();
+var
+  nodeCurr: TOperationListNode;
+begin
+  writeln(self.count, ' элементов в списке:');
+  nodeCurr := self.head;
+  while nodeCurr <> nil do
+  begin
+    writeln('id: ',nodeCurr.item^.id);
+    write('  тип: ');
+    case nodeCurr.item^.tp of
+      income: writeln('доход');
+      outcome: writeln('расход');
+    end;
+    writeln('  сумма: ', nodeCurr.item^.money div 100,
+      ' руб. ', nodeCurr.item^.money mod 100, ' коп.');
+    writeln('  id хранилища: ', nodeCurr.item^.storageId);
+    writeln('  id категории: ', nodeCurr.item^.categoryId);
+    writeln('  дата: ', dateToStr(nodeCurr.item^.date));
+    writeln('  описание: ', nodeCurr.item^.description);
+    nodeCurr := nodeCurr.next;
+  end;
+end;
+
+end.
