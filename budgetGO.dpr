69852f95d5de601a2a7ff9c139abfc830fa1c95b
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-03-10 15:35:56 +0300

    New project

---
+++ budgetGO.dpr
@@ -0,0 +1,17 @@
+program budgetGO;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+begin
+  try
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
