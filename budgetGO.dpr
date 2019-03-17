78fd82ff561706742f22ebc26dedc415894cb539
oleg-grigorijan <oleg.grigorijan@gmail.com>
2019-03-17 14:37:42 +0300

    TCategory and TCategoryTable are created

--- budgetGO.dpr
+++ budgetGO.dpr
@@ -8,7 +8,9 @@ uses
   System.SysUtils,
   UnitTOperation in 'models\UnitTOperation.pas',
   UnitTOperationListNode in 'models\UnitTOperationListNode.pas',
-  UnitTOperationList in 'models\UnitTOperationList.pas';
+  UnitTOperationList in 'models\UnitTOperationList.pas',
+  UnitTCategory in 'models\UnitTCategory.pas',
+  UnitTCategoryTable in 'models\UnitTCategoryTable.pas';
 
 const
   dataDName = 'data';
@@ -24,7 +26,15 @@ begin
     if not directoryExists(dataDName) then
       createDir(dataDName);
     operList := TOperationList.create(operFName);
-
+    //{
+    for i := 0 to 3 do
+    begin
+      new(operTmp);
+      operTmp^.id := operList.getItemsMaxId + 1;
+      operList.addNode(operTmp);
+    end;
+    //}
+    operlist.consoleOutput;
     readln;
 
     operList.destroy()
